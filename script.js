b64db150232693786936f86d64d5fe12707e3ea4
Aditya <88152270+AdityaS256@users.noreply.github.com>
2023-06-22 13:48:21 +0530

    Add buttons and drawability

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let size = prompt("Size eg.(16 = 16x16 grid)");
 
-let canvas = document.querySelector('.canvas');
+let canvas = document.querySelector(".canvas");
 
 let attribute = `repeat(${size}, 1fr)`;
 
@@ -21,9 +21,114 @@ if (availableVerticalSpace < availableHorizontalSpace) {
 size = size * size;
 
 for (let i = 1; i <= size; i++) {
-    let pixel = document.createElement('div');
+  let pixel = document.createElement("div");
 
-    pixel.classList.add('pixel');
+  pixel.classList.add("pixel");
 
-    canvas.appendChild(pixel);
+  canvas.appendChild(pixel);
 }
+
+let eraseMode = false;
+
+function grid() {
+  let pixels = document.querySelectorAll(".pixel");
+
+  let toggle = document.querySelector("#toggle");
+
+  if (toggle.textContent === "grid_on") {
+    pixels.forEach(function (pixel) {
+      pixel.style.borderStyle = "none";
+    });
+
+    toggle.textContent = "grid_off";
+  } else {
+    pixels.forEach(function (pixel) {
+      pixel.style.borderStyle = "solid";
+    });
+
+    toggle.textContent = "grid_on";
+  }
+}
+
+function clear() {
+  let pixels = document.querySelectorAll(".pixel");
+
+  pixels.forEach(function (pixel) {
+    pixel.style.backgroundColor = "#FFFFFF";
+  });
+}
+
+let clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', clear);
+
+function draw () {
+  let pixels = document.querySelectorAll(".pixel");
+
+  let isDrawing;
+
+  canvas.addEventListener('mousedown', function () {
+    isDrawing = true;
+  });
+
+  canvas.addEventListener('mouseup', function () {
+    isDrawing = false;
+  });
+
+  Array.from(pixels).forEach(function (pixel) {
+    pixel.addEventListener('mousemove', function (event) {
+      if (isDrawing) {
+        if (eraseMode) {
+          paint(event.target, "white");
+        } else {
+          let color = document.querySelector('#color').value;
+          paint(event.target, color);
+        }
+      }
+    });
+  });
+}
+
+function paint (pixel, color) {
+  pixel.style.backgroundColor = color;
+}
+
+draw();
+
+function eraser () {
+  let eraseButton = document.querySelector('#eraser');
+
+  if (eraseMode === false) {
+    eraseButton.style.backgroundColor = "rgba(16, 185, 129, 0.25)";
+    eraseButton.style.color = "rgba(16, 185, 129)";
+
+    eraseButton.addEventListener('mouseover', function () {
+      eraseButton.style.backgroundColor = "rgba(16, 185, 129, 0.5)";
+    });
+
+    eraseButton.addEventListener('mouseout', function () {
+      eraseButton.style.backgroundColor = "rgba(16, 185, 129, 0.25)";
+    });
+
+    eraseMode = true;
+  } else {
+    eraseButton.style.backgroundColor = "rgba(217, 119, 6, 0.25)";
+    
+    eraseButton.addEventListener('mouseover', function () {
+      eraseButton.style.backgroundColor = "rgba(217, 119, 6, 0.5)";
+    });
+    
+    eraseButton.addEventListener('mouseout', function () {
+      eraseButton.style.backgroundColor = "rgba(217, 119, 6, 0.25)";
+    });
+
+    eraseButton.style.color = "rgba(217, 119, 6)";
+    eraseMode = false;
+  }
+}
+
+let colorPicker = document.querySelector('#color');
+
+colorPicker.addEventListener('input', function (event) {
+  event.target.style.backgroundColor = event.target.value;
+});
